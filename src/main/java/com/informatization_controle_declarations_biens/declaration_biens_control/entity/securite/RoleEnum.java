26f33acca697fc7fb501438c9956104ef21bc6f4
AnibaSoumaya <mayaaniba98@gmail.com>
2025-06-02 05:55:35 +0100

    role-update

--- src/main/java/com/informatization_controle_declarations_biens/declaration_biens_control/entity/securite/RoleEnum.java
+++ src/main/java/com/informatization_controle_declarations_biens/declaration_biens_control/entity/securite/RoleEnum.java
@@ -2,12 +2,8 @@ package com.informatization_controle_declarations_biens.declaration_biens_contro
 
 public enum RoleEnum {
     administrateur,
-    premier_president,
     conseiller_rapporteur,
-    verificateur,
-    greffier_chef,
     procureur_general,
-    president_quatrieme_champbre,
     avocat_general,
 
 
